cd38eee02e54a8a908f7e59ece81cea042b99848
David Foster <david@apache.co.uk>
2019-06-05 16:45:50 +0100

    New approach which only updates floor plane if necessary. Better naming for error margin.

--- Projects/MagicLeapFloorDetection/Assets/FloorDetection/Scripts/FloorDetectionController.cs
+++ Projects/MagicLeapFloorDetection/Assets/FloorDetection/Scripts/FloorDetectionController.cs
@@ -10,7 +10,7 @@ public class FloorDetectionController : MonoBehaviour {
 	//-----------------------------------------------------------------------------------------
 
 	private const float DEFAULT_FLOOR_MEASUREMENT_MAX_AGE = 2;
-	private const float DEFAULT_FLOOR_PLANE_Y_SHADER_ERROR_MARGIN = 0.05f;
+	private const float DEFAULT_FLOOR_PLANE_Y_MESHING_ERROR_MARGIN = 0.05f;
 
 	private const float DEFAULT_BOUNDS_SIZE = 10;
 
@@ -22,12 +22,14 @@ public class FloorDetectionController : MonoBehaviour {
 
 	[Space]
 
-	[Tooltip("Floor measurements are averaged over time. This value determines the duration in seconds that those measurements factor into our average.")]
+	[Tooltip("Floor measurements are averaged over time. This value determines the duration in seconds that those measurements factor into " +
+	         "our average.")]
 	[SerializeField] protected float floorMeasurementMaxAge = DEFAULT_FLOOR_MEASUREMENT_MAX_AGE;
 
-	[Tooltip("Additional margin used to envelop noise in the spatial map which falls just above the detected floor y but should still be considered part" +
-	         "of the floor. This value only effects shaders; the " + nameof(FloorY) + " property of this component denotes the de facto floor y.")]
-	[SerializeField] protected float floorPlaneYShaderErrorMargin = DEFAULT_FLOOR_PLANE_Y_SHADER_ERROR_MARGIN;
+	[Tooltip("Additional margin used to envelop noise in the spatial map which falls just above the detected floor y but should still be " +
+	         "considered part of the floor. This value is only used with the ML Meshing/Raycast API; the " + nameof(FloorY) + " property of " +
+	         "this component denotes the de facto floor y.")]
+	[SerializeField] protected float floorPlaneYMeshingErrorMargin = DEFAULT_FLOOR_PLANE_Y_MESHING_ERROR_MARGIN;
 
 	[Space]
 
@@ -45,6 +47,10 @@ public class FloorDetectionController : MonoBehaviour {
 	// Private Fields:
 	//-----------------------------------------------------------------------------------------
 
+	/// <summary>The current plane describing the normal and distance of our working floor plane.</summary>
+	/// <remarks>This is not to be confused with a Magic Leap Planes API <c>MLWorldPlane</c>.</remarks>
+	private Plane currFloorPlane;
+
 	private readonly Queue<FloatMeasurement> floorYMeasurements = new Queue<FloatMeasurement>();
 
 	//-----------------------------------------------------------------------------------------
@@ -52,7 +58,7 @@ public class FloorDetectionController : MonoBehaviour {
 	//-----------------------------------------------------------------------------------------
 
 	protected void Awake() {
-
+		
 		// set our scale to determine bounds used by Magic Leap Planes API.
 		transform.localScale = Vector3.one * boundsSize;
 	}
@@ -60,20 +66,26 @@ public class FloorDetectionController : MonoBehaviour {
 	protected void Update() {
 
 		// handle updating the global floor plane shader data.
-		// N.B. we could technically only do this when our floor position changes in Planes_Updated below, but by doing this in Update, and with the
-		// ExecuteInEditMode attribute above, we can update our shaders with floor plane info at edit time by moving our transform.
-		
-		// create a plane at the floor position (plus padding), using our up as its normal.
+		// N.B. we could technically only do this when our floor position changes in Planes_Updated below, but by doing this in Update, and
+		// with the ExecuteInEditMode attribute above, we can update our shaders with floor plane info at edit time by moving our transform.
+
+		// grab the current plane's values prior to recalculation so we can later see if it has changed.
+		// N.B. we can do this because Unity's Plane is a struct so prevFloorPlane is a new instance with currFloorPlanes values.
+		Plane prevFloorPlane = currFloorPlane;
+
+		// create a new plane at the floor position (plus error margin), using our up as its normal.
 		// N.B. the x and z of the floor plane is not important since its x and z extend infinitely orthogonal to the normal.
 		Vector3 floorPlanePosition = transform.position;
-		floorPlanePosition.y = FloorY + floorPlaneYShaderErrorMargin;
-		Plane plane = new Plane(transform.up, floorPlanePosition);
+		floorPlanePosition.y = FloorY + floorPlaneYMeshingErrorMargin;
+		currFloorPlane = new Plane(transform.up, floorPlanePosition);
 
-		// create a Vector4 representation of the plane that we can pass into our shader.
-		Vector4 planeRepresentation = new Vector4(plane.normal.x, plane.normal.y, plane.normal.z, plane.distance);
+		// back out if we don't detect a change in floor plane.
+		// N.B. frustratingly, at least as of 2019.1.5f1, Unity have yet to implement IEquatable on Planes.
+		if (currFloorPlane.normal == prevFloorPlane.normal && currFloorPlane.distance == prevFloorPlane.distance) return;
 
-		// pass the plane vector into every shader globally.
-		Shader.SetGlobalVector(FLOOR_PLANE_GLOBAL_SHADER_PROPERTY, planeRepresentation);
+		// create a Vector4 representation of the plane and pass it into all shaders globally.
+		Vector4 plane = new Vector4(currFloorPlane.normal.x, currFloorPlane.normal.y, currFloorPlane.normal.z, currFloorPlane.distance);
+		Shader.SetGlobalVector(FLOOR_PLANE_GLOBAL_SHADER_PROPERTY, plane);
 	}
 
 	//-----------------------------------------------------------------------------------------
